33983d6d3200f09a0a0ae8a924df9bd4aa850800
lideshou <1427068168@qq.com>
2019-06-11 21:01:09 +0800

    project picture

--- client/src/views/Fundlist.vue
+++ client/src/views/Fundlist.vue
@@ -26,31 +26,31 @@
         </div>
         <div class="tableContainer">
             <el-table v-if="tableData.length>0" :data="tableData" max-height="450" border black style="width: 100%">
-                <el-table-column type="index" label="序号" align="center" width="50"></el-table-column>
+                <el-table-column type="index" label="序号" align="center" width="100"></el-table-column>
                 <el-table-column prop="date" label="创建时间" align="center" width="250">
                     <template slot-scope="scope">
                         <i class="el-icon-time"></i>
                         <span style="margin-left: 10px">{{ scope.row.date }}</span>
                     </template>
                 </el-table-column>
-                <el-table-column prop="type" label="收支类型" align="center" width="100"></el-table-column>
-                <el-table-column prop="describe" label="收支描述" align="center" width="150"></el-table-column>
-                <el-table-column prop="income" label="收入" align="center" width="70">
+                <el-table-column prop="type" label="收支类型" align="center" width="150"></el-table-column>
+                <el-table-column prop="describe" label="收支描述" align="center" width="250"></el-table-column>
+                <el-table-column prop="income" label="收入" align="center" width="120">
                     <template slot-scope="scope">
                         <span style="color:#00d053">{{ scope.row.income }}</span>
                     </template>
                 </el-table-column>
-                <el-table-column prop="expend" label="支出" align="center" width="70">
+                <el-table-column prop="expend" label="支出" align="center" width="120">
                     <template slot-scope="scope">
                         <span style="color:#f56767">{{ scope.row.expend }}</span>
                     </template>
                 </el-table-column>
-                <el-table-column prop="cash" label="账户现金" align="center" width="70">
+                <el-table-column prop="cash" label="账户现金" align="center" width="120">
                     <template slot-scope="scope">
                         <span style="color:#4db3ff">{{ scope.row.cash }}</span>
                     </template>
                 </el-table-column>
-                <el-table-column prop="remark" label="备注" align="center" width="200"></el-table-column>
+                <el-table-column prop="remark" label="备注" align="center" width="400"></el-table-column>
                 <el-table-column label="操作" prop="operation" align="center" fixed="right" width="150" v-if="user.identity=='manager'">
                     <template slot-scope="scope">
                         <el-button type="warning" icon="edit" size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
